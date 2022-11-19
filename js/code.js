1f9ac499162cea4b05867c81d59eb3e8bcdf8410
Gift Mahlangu <giftnkz6@gmail.com>
2022-11-20 01:54:20 +0200

    Added style

--- js/code.js
+++ js/code.js
@@ -1,2 +1,22 @@
 let buttons = document.querySelectorAll(".btn");
-let screen = document.querySelector(".screen");
+let screen = document.getElementById("screen");
+
+// buttons.addEventListener('click', () => {
+//     alert("Hello Hi");
+// });
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", function(e) {
+        // alert("Ngamandla");
+        let display = document.getElementsByClassName("btn").textContent;
+    
+        screen.innerHTML += display;
+        
+
+
+
+    });
+
+}
+// function clicked() {
+//     alert("Hello Hi");
+// }
